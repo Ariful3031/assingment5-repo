6126df38503958c77e9e99e711b709fced181b18
unknown <hero.ariful31@gmail.com>
2025-08-27 22:15:10 +0600

    added to complete responsive

--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -1,1 +1,12 @@
 
+// tailwind.config.js
+module.exports = {
+    theme: {
+        extend: {
+            screens: {
+                'xs': '480px',
+                '3xl': '1920px',
+            },
+        },
+    },
+}
