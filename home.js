ee96eb8f62c3292863465ef64d8013c9e93bfdbd
unknown <hero.ariful31@gmail.com>
2025-08-27 14:04:28 +0600

    added heart js funtcion

---
+++ home.js
@@ -0,0 +1,140 @@
+
+
+//   add money features Starts
+// const addPin = 1234;
+// document.getElementById("btn-added-money").addEventListener("click", function (event) {
+//     event.preventDefault()
+//     const selectBank = getInputValue("add-select-bank");
+//     const addNumber = getInputValue("add-number")
+//     if (addNumber.length !== 11) {
+//         alert('please set valid number')
+//         return;
+//     }
+//     const addPinNumber = getInputValueNumber("add-pin-number");
+//     if (addPin !== addPinNumber) {
+//         alert("please set valid pin number")
+//         return;
+//     }
+//     const addAmount = getInputValueNumber('add-amount');
+//     if (addAmount <= 0) {
+//         alert("invalid amount")
+//         return;
+//     }
+//     const mainAvailableBalance = getInputInnerTextNumber("main-available-balance");
+//     const newMainAvailableBalance = mainAvailableBalance + addAmount;
+//     document.getElementById("main-available-balance").innerText = newMainAvailableBalance;
+
+//     const data = {
+//         name: "Add Money",
+//         date: new Date().toLocaleTimeString()
+//     }
+//     transactionsData.push(data);
+// })
+
+
+
+
+// button one Heart
+document.getElementById("btn-heart-one").addEventListener("click", function (event) {
+    event.preventDefault()
+    const mainAmount = parseInt(document.getElementById("heart-main-amount").innerText)
+    const newAmount = mainAmount + 1;
+    document.getElementById('heart-main-amount').innerText = newAmount;
+
+})
+
+// button two Heart
+document.getElementById("btn-heart-two").addEventListener("click", function (event) {
+    event.preventDefault()
+    const mainAmount = parseInt(document.getElementById("heart-main-amount").innerText)
+    const newAmount = mainAmount + 1;
+    document.getElementById('heart-main-amount').innerText = newAmount;
+
+})
+
+// button three Heart
+document.getElementById("btn-heart-three").addEventListener("click", function (event) {
+    event.preventDefault()
+    const mainAmount = parseInt(document.getElementById("heart-main-amount").innerText)
+    const newAmount = mainAmount + 1;
+    document.getElementById('heart-main-amount').innerText = newAmount;
+
+})
+// button four Heart
+
+
+
+// button five Heart
+document.getElementById("btn-heart-five").addEventListener("click", function (event) {
+    event.preventDefault()
+    const mainAmount = parseInt(document.getElementById("heart-main-amount").innerText)
+    const newAmount = mainAmount + 1;
+    document.getElementById('heart-main-amount').innerText = newAmount;
+
+})
+
+// button six Heart
+document.getElementById("btn-heart-six").addEventListener("click", function (event) {
+    event.preventDefault()
+    const mainAmount = parseInt(document.getElementById("heart-main-amount").innerText)
+    const newAmount = mainAmount + 1;
+    document.getElementById('heart-main-amount').innerText = newAmount;
+
+})
+
+// button seven Heart
+document.getElementById("btn-heart-seven").addEventListener("click", function (event) {
+    event.preventDefault()
+    const mainAmount = parseInt(document.getElementById("heart-main-amount").innerText)
+    const newAmount = mainAmount + 1;
+    document.getElementById('heart-main-amount').innerText = newAmount;
+
+})
+// button eight Heart
+document.getElementById("btn-heart-eight").addEventListener("click", function (event) {
+    event.preventDefault()
+    const mainAmount = parseInt(document.getElementById("heart-main-amount").innerText)
+    const newAmount = mainAmount + 1;
+    document.getElementById('heart-main-amount').innerText = newAmount;
+
+})
+
+// button nine Heart
+document.getElementById("btn-heart-nine").addEventListener("click", function (event) {
+    event.preventDefault()
+    const mainAmount = parseInt(document.getElementById("heart-main-amount").innerText)
+    const newAmount = mainAmount + 1;
+    document.getElementById('heart-main-amount').innerText = newAmount;
+
+})
+// button ten Heart
+document.getElementById("btn-heart-ten").addEventListener("click", function (event) {
+    event.preventDefault()
+    const mainAmount = parseInt(document.getElementById("heart-main-amount").innerText)
+    const newAmount = mainAmount + 1;
+    document.getElementById('heart-main-amount').innerText = newAmount;
+
+})
+
+
+
+
+
+
+
+
+// document.getElementById("btn-heart-ten").addEventListener("click", function (event) {
+//     event.preventDefault()
+//     const mainAmount = parseInt(document.getElementById("heart-main-amount").innerText)
+//     const newAmount = mainAmount + 1;
+//     document.getElementById('heart-main-amount').innerText = newAmount;
+
+// })
+
+
+
+
+
+
+
+
